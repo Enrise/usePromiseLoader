8ad643f96be015744f97d2d3ce70a4d6dafb8d3b
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2020-09-14 12:19:40 +0200

    Fix loading bug

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const usePromiseLoader = <DataType>(
         .catch((error) => setError(error));
       return;
     }
-    setData(data);
+    setData(dataFromProp);
     setLoading(false);
   }, [dataFromProp]);
 
